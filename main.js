eb89e9173f981fad79031c2d6ef683a80c79827a
abdirsaq <abdirsaknorosman@gmail.com>
2023-03-11 17:07:32 +0300

    some changes

--- main.js
+++ main.js
@@ -15,3 +15,19 @@ btn_2.addEventListener('click', (event) => {
 })
 
 
+
+
+// ........................... menu bar
+const btn_bar = document.querySelector('.btn_bar')
+const nav_list  =document.querySelector('#open')
+
+
+btn_bar.addEventListener('click', () => {
+        if(nav_list.style.display == 'none'){
+                nav_list.style.display = "block"
+                nav_list.style.marginLeft = '160px'
+                nav_list.style.transition = '0.5s all'
+        }else{
+                nav_list.style.display = 'none'
+        }
+})
